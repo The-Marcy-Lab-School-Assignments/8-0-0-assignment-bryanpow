d97456b20fe1986d6b7298ed7c8a027007839e6e
bryanpow <ramosbusiness544@gmail.com>
2024-04-09 15:31:01 -0400

    all paths

--- server/controllers/assets-controller.js
+++ server/controllers/assets-controller.js
@@ -1,7 +1,7 @@
 path = require("path");
 exports.about = (req, res) => {
     try {
-        res.sendFile(path.join(__dirname, "assets", "about.html"));
+        res.sendFile(path.join(__dirname, "../assets", "about.html"));
     } catch (err) {
         console.error("Error sending about page:", err);
         res.status(500).send("Internal server error");
--- server/index.js
+++ server/index.js
@@ -11,9 +11,9 @@ const {about, footer} = require('./controllers/assets-controller')
 app.get("/about", about)
 app.get("/footer", footer)
 
-app.get("api/posts", getPosts);
+app.get("/api/posts", getPosts);
 
-app.get("api/users", getUser);
+app.get("/api/users", getUser);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
